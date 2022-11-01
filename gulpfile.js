b74f011d823400113e41381a26cd2a7b6396682d
Irina Shebarshina <iv.shebarshina@gmail.com>
2022-11-02 02:29:39 +0300

    task copy

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+// основной модуль
+import gulp from "gulp";
+import {path} from "./gulp/config/path.js"
+
+global.app = {
+    path: path,
+    gulp: gulp,
+}
+
+import {copy} from "./gulp/tasks/copy.js";
+
+gulp.task('default', copy);
